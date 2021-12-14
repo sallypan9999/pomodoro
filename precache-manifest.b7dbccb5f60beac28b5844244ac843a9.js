1d696948d909ebd11ff6d6f0f473897125f10336
sallypan9999 <90738794+sallypan9999@users.noreply.github.com>
2021-12-14 00:22:55 +0000

    Deploying to gh-pages from @ sallypan9999/pomodoro@0b9a7f26acdfeba0bca250edfa81d0080d312c80 🚀

---
+++ precache-manifest.b7dbccb5f60beac28b5844244ac843a9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c3ea2a8e805053e41e",
+    "url": "css/app.deb39008.css"
+  },
+  {
+    "revision": "69d81e8932d41732ab0b",
+    "url": "css/chunk-vendors.989680b1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dea9842b1b34def05c94e10c636cddbb",
+    "url": "img/tomato.png"
+  },
+  {
+    "revision": "b9da24343399336ea344a8c754f58aa4",
+    "url": "index.html"
+  },
+  {
+    "revision": "95c3ea2a8e805053e41e",
+    "url": "js/app.34ae6f80.js"
+  },
+  {
+    "revision": "69d81e8932d41732ab0b",
+    "url": "js/chunk-vendors.0f8ae757.js"
+  },
+  {
+    "revision": "9d809ea8017734f9b2a8",
+    "url": "js/list.f5993151.js"
+  },
+  {
+    "revision": "6267ef74889582abdc5a",
+    "url": "js/notfound.bf556353.js"
+  },
+  {
+    "revision": "59a2c5adce4b1dc23090",
+    "url": "js/settings.c0d8beb5.js"
+  },
+  {
+    "revision": "cbf815ef04c07edbb8b9441686163a6f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "media/alarm.965b614d.mp3"
+  },
+  {
+    "revision": "46c744f5e53a2259c87f4b029cb43e03",
+    "url": "media/yay.46c744f5.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
